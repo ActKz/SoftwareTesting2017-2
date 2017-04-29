79bb2f9a5bd6d2cf6d0f6e9eee4f989421f91082
actkz <actkz.cs02@g2.nctu.edu.tw>
2017-04-29 19:25:46 +0800

    Update test

--- tests/test_commission.py
+++ tests/test_commission.py
@@ -3,12 +3,6 @@ from ..hw2.Commission import commission
 
 
 @pytest.mark.parametrize("locks, stocks, barrels, expected",[
-        ( 0, 80, 90, "Out of range value"),
-        ( 70, 0, 90, "Out of range value"),
-        ( 70, 80, 0, "Out of range value"),
-        ( 1, 1, 1, 10),
-        ( 11, 10, 10, 106.75),
-        ( 70, 80, 90, 1420),
     ])
 def test_c0( locks, stocks, barrels, expected):
     assert commission( locks, stocks, barrels) == expected
@@ -24,6 +18,12 @@ def test_c2( locks, stocks, barrels, expected):
     assert commission( locks, stocks, barrels) == expected
 
 @pytest.mark.parametrize("locks, stocks, barrels, expected",[
+        ( 0, 80, 90, "Out of range value"),
+        ( 70, 0, 90, "Out of range value"),
+        ( 70, 80, 0, "Out of range value"),
+        ( 1, 1, 1, 10),
+        ( 11, 10, 10, 106.75),
+        ( 70, 80, 90, 1420),
     ])
 def test_mcdc( locks, stocks, barrels, expected):
     assert commission( locks, stocks, barrels) == expected
